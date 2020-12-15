35df1c078f191890423ba33bb44607567e954115
Jishan2610 <jishan4all@gmail.com>
2020-12-15 18:34:38 +0530

    my first commit reactwebsite

---
+++ README.md
@@ -0,0 +1,15 @@
+## Weather App w/ React
+
+- Project (CRA) cleanup
+- API calls
+- Search 
+- Movie Info
+
+API Stuff
+const api = {
+  key: "03561e1b3667cdbdfc6b076a78dc7113",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+Responsive React Weather App and easy to use 
+Made with Love
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+const api = {
+  key: "03561e1b3667cdbdfc6b076a78dc7113",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  // const search = evt => {
+  //   if (evt.key === "Enter") {
+  //     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  //       .then(res => res.json())
+  //       .then(result => {
+  //         setWeather(result);
+  //         setQuery('');
+  //         console.log(result);
+  //       });
+  //   }
+  // }
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+}
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} , ${year}`
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app sunny' : 'app') : 'app'}>
+      <main>
+        <div className="search_box">
+          <form onSubmit={onSubmitHandler}>
+          <input 
+            type="text"
+            className="search_bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            // onKeyPress={search}
+          />
+        </form>
+        </div>
+        {( typeof weather.main !== "undefined") ? (
+        <div className="below_search">
+          <div className="location_box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather_box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="temp_container">
+              <div className="min_temp">{Math.round(weather.main.temp_min)}°c</div>
+              <div className="min_tem"></div>
+              <div className="max_temp">{Math.ceil(weather.main.temp_max)}°c</div>
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
